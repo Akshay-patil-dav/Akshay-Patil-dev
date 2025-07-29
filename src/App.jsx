67312b8fcbff6a8915cb8dd1334d4ee8f9ffa0ac
Akshay Patil <akshaypatil22082002@gmail.com>
2025-07-29 22:56:42 +0530

    update
    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main_header from "./Components/Header/Main_header";
-import Home from "./Pages/Client/Home";
-import Dashboard from "./Pages/Admin/Dashboard";
-import Main_nav from "./Components/Header/Main_nav";
 import HomePage from "./Pages/Portfolio/HomePage";
 import Headers from "./Components/Header/Header";
 
@@ -11,17 +7,7 @@ export default function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main_header />}>
-            <Route index element={<Home />} />
-            {/* Add more routes here as needed */}
-          </Route>
-
-          <Route path="/home" element={<Main_nav />}>
-            <Route index element={<Dashboard />} />
-            {/* Add more admin routes here */}
-          </Route>
-
-          <Route path="/portfolio" element={<Headers />}>
+          <Route path="/" element={<Headers />}>
             <Route index element={<HomePage />} />
             {/* Add more admin routes here */}
           </Route>
