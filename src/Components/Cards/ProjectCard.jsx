776990a1250a442b3cef5f52fd6e022b2bf2fe3e
Akshay Patil <akshaypatil22082002@gmail.com>
2025-09-26 15:37:49 +0530

    Update ProjectCard.jsx

--- src/Components/Cards/ProjectCard.jsx
+++ src/Components/Cards/ProjectCard.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useState } from "react";
 
 export default function ProjectCard() {
+     const allItems = [
+    { id: 1, color: "red", type: "fruit" },
+    { id: 2, color: "green", type: "vegetable" },
+    { id: 3, color: "yellow", type: "fruit" },
+    { id: 4, color: "orange", type: "vegetable" },
+  ];
+
+  const [filter, setFilter] = useState("all");
+
+  const filteredItems =
+    filter === "all" ? allItems : allItems.filter((item) => item.type === filter);
+
     return (
         <>
 
@@ -23,20 +36,38 @@ export default function ProjectCard() {
                     </div>
                 </div>
             </div>
-            <section className='container' style={{position:"relative", bottom:"100px"}}>
-                <nav>
-                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-                        <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
-                        <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab" aria-controls="nav-disabled" aria-selected="false" disabled>Disabled</button>
-                    </div>
-                </nav>
-                <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">...</div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">...</div>
-                    <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab" tabindex="0">...</div>
+            <section className='container' style={{ position: "relative", bottom: "100px" }}>
+                <ul class="nav nav-pills mb-4" id="pillNav" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="nav-home-tab" onClick={() => setFilter("all")} data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">All</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="nav-profile-tab" onClick={() => setFilter("fruit")} data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Front-End</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="nav-contact-tab" onClick={() => setFilter("vegetable")} data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Back-End</button>
+                    </li>
+                </ul>
+
+                {/* Display boxes */}
+                <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
+                    {filteredItems.map((item) => (
+                        <div
+                            key={item.id}
+                            style={{
+                                width: "80px",
+                                height: "80px",
+                                background: item.color,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                color: "white",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            {item.type}
+                        </div>
+                    ))}
                 </div>
             </section>
 
