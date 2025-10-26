0d9a082085190262474746a9cb758d3df983e9bb
Akshay Patil <akshaypatil22082002@gmail.com>
2025-10-26 17:53:03 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Akshay-Patil-dev/dist/',
+  base: '/Akshay-Patil-dev/',
   plugins: [react()],
 })
